7f09e87959c8d080cb49ef2a421817ff254ea26d
thuangodcode <thuanntse183670@fpt.edu.vn>
2025-09-28 20:35:00 +0700

    Fix port for Render

--- be/index.js
+++ be/index.js
@@ -3,7 +3,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Swagger config
 const swaggerOptions = {
